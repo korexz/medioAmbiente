4ff1bf1d8097e60af0b8f2e91baa96245dee2c5a
Mac <marco.a.stevenson@gmail.com>
2022-07-22 17:46:04 -0400

    cambios en todos lados

---
+++ medio ambiente/js/remplazar-img.js
@@ -0,0 +1,9 @@
+//intento de function para reemplzar una imagen desde la pagina principal
+
+
+// hacer una funcion para recuperar la imagen que esta activa en pagina princiapal
+
+//cuando esa imagen este recuperada de la pagina principal hay que remplezarla en la pagina de destino => trabajo1 y para todas las que estaran
+
+// ver la forma de que se reemeplazee donde sea sin importar cambiar el nombre del html
+
